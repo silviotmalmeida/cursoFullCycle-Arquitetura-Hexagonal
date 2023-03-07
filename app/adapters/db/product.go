61b8c396f66525412bfd27157436a5199b6efa92
silviotmalmeida <silviotmalmeida@gmail.com>
2023-03-07 18:19:26 -0300

    Implementação ate aula 18

---
+++ app/adapters/db/product.go
@@ -0,0 +1,120 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/silviotmalmeida/cursoFullCycle-Arquitetura-Hexagonal/application"
+)
+
+// definindo a classe productDb que implementa a interface de sql
+type ProductDb struct {
+	db *sql.DB
+}
+
+// definindo o construtor
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
+// implementando o método Get da interface productReader, utilizada na productPersistenceInterface
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+
+	// criando o product
+	var product application.Product
+
+	// preparando o sql
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
+	// em caso de erro, retorna-o
+	if err != nil {
+		return nil, err
+	}
+
+	// executando a query e relacionando o resultado aos atributos do product criado anteriormente
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	// em caso de erro, retorna-o
+	if err != nil {
+		return nil, err
+	}
+
+	// retorna o product populado
+	return &product, nil
+}
+
+// implementando o método Save da interface productWriter, utilizada na productPersistenceInterface
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+
+	// inicializando a variável para verificação de existência de registros
+	var rows int
+
+	// executando a query para verificação de existência de registro e populando a variável de quantitativo de registros
+	p.db.QueryRow("Select id from products where id=?", product.GetID()).Scan(&rows)
+
+	// se não existirem registros
+	if rows == 0 {
+		// realiza a criação
+		_, err := p.create(product)
+		// em caso de erro, retorna-o
+		if err != nil {
+			return nil, err
+		}
+		// se existir registro
+	} else {
+		// realiza a atualização
+		_, err := p.update(product)
+		// em caso de erro, retorna-o
+		if err != nil {
+			return nil, err
+		}
+	}
+	// retorna o product
+	return product, nil
+}
+
+// implementando o método de criação
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+
+	// preparando o sql
+	stmt, err := p.db.Prepare(`insert into products(id, name, price, status) values(?,?,?,?)`)
+	// em caso de erro, retorna-o
+	if err != nil {
+		return nil, err
+	}
+
+	// executando a query
+	_, err = stmt.Exec(
+		product.GetID(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+	// em caso de erro, retorna-o
+	if err != nil {
+		return nil, err
+	}
+
+	// encerrando a transação
+	err = stmt.Close()
+	// em caso de erro, retorna-o
+	if err != nil {
+		return nil, err
+	}
+
+	// retorna o product
+	return product, nil
+}
+
+// implementando o método de atualização
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+
+	// executando a query
+	_, err := p.db.Exec("update products set name = ?, price=?, status=? where id = ?",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetID())
+	// em caso de erro, retorna-o
+	if err != nil {
+		return nil, err
+	}
+
+	// retorna o product
+	return product, nil
+}
--- app/application/product_service_test.go
+++ app/application/product_service_test.go
@@ -38,35 +38,62 @@ func TestProductService_Get(t *testing.T) {
 
 //// se um registro for armazenado no bd, seus atributos devem ser iguais aos do objeto de origem
 func TestProductService_Create(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	product := mock_application.NewMockProductInterface(ctrl)
-	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
-	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
-	service := application.ProductService{Persistence: persistence}
 
+	// criando o controlador do gomock
+	ctrl := gomock.NewController(t)
+	// o controlador será finalizado ao fim do teste
+	defer ctrl.Finish()
+
+	// criando o mock de product
+	product := mock_application.NewMockProductInterface(ctrl)
+	// criando o mock do mecanismo de persistência
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	// sempre que método Save do mecanismo de interface for chamado, retorna um product
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	// criando o productService a ser testado
+	service := application.ProductService{Persistence: persistence}
+	// realizando a criação pelo service
 	result, err := service.Create("Product 1", 10)
+	// não pode retornar erro
 	require.Nil(t, err)
+	// deve retornar um product igual ao mock
 	require.Equal(t, product, result)
 }
 
 //// se um product for ativado/desativado no bd, seus atributos devem ser iguais aos do objeto de origem
 func TestProductService_EnableDisable(t *testing.T) {
+
+	// criando o controlador do gomock
 	ctrl := gomock.NewController(t)
+	// o controlador será finalizado ao fim do teste
 	defer ctrl.Finish()
+
+	// criando o mock de product
 	product := mock_application.NewMockProductInterface(ctrl)
+	// retorna nil ao ativar
 	product.EXPECT().Enable().Return(nil)
+	// retorna nil ao desativar
 	product.EXPECT().Disable().Return(nil)
 
+	// criando o mock do mecanismo de persistência
 	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	// sempre que método Save do mecanismo de interface for chamado, retorna um product
 	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
-	service := application.ProductService{Persistence: persistence}
 
+	// criando o productService a ser testado
+	service := application.ProductService{Persistence: persistence}
+	// realizando a ativação pelo service
 	result, err := service.Enable(product)
+	// não pode retornar erro
 	require.Nil(t, err)
+	// deve retornar um product igual ao mock
 	require.Equal(t, product, result)
 
+	// realizando a desativação pelo service
 	result, err = service.Disable(product)
+	// não pode retornar erro
 	require.Nil(t, err)
+	// deve retornar um product igual ao mock
 	require.Equal(t, product, result)
 }
